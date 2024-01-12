3d601a4ed1a2e6646a549602a38dbc6869ccb2f0
zoolotoui <chag.med.amine@gmail.com>
2024-01-12 17:14:43 +0100

    update

--- view/categories.php
+++ view/categories.php
@@ -10,6 +10,35 @@
 </head>
 <body class="bg-blueGray-50">
 <?php include 'includes/nav.php'; ?>
+<section class="relative pt-16 bg-blueGray-50">
+    <div class="container mx-auto text-center">
+            <h1 class="text-4xl font-semibold mb-2 text-blue-600">Welcome to our categories</h1>
+            <p class="text-lg text-gray-400 mb-8">Our top categories</p>
+        </div>
+<div class="container mx-auto">
+  
+<div class="grid grid-cols-2 gap-16">
+<?php
+include_once '../controller/category.contr.php';
+foreach($categories as $cat){?>
+    <a href="categories.php?cat_id=<?php echo $cat['cat_id']; ?>" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <img class="object-cover w-full rounded-t-lg h-48 md:h-96 md:w-48 md:rounded-none md:rounded-s-lg" src="data:image/jpg;charset=utf8;base64,<?=  base64_encode($cat['image']); ?>" alt="">
+        <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><?php echo $cat['cat_name']; ?></h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+    </a>
+<?php
+}
+?>
+
+
+
+</div>
+
+</div>
+
+</section>
     
 </body>
 </html>
--- view/index.php
+++ view/index.php
@@ -28,7 +28,7 @@ include '../controller/wiki.contr.php';
 <?php
 include_once '../controller/category.contr.php';
 foreach($fourcategories as $cat){?>
-    <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <a href="categories.php" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
         <img class="object-cover w-full rounded-t-lg h-48 md:h-96 md:w-48 md:rounded-none md:rounded-s-lg" src="data:image/jpg;charset=utf8;base64,<?=  base64_encode($cat['image']); ?>" alt="">
         <div class="flex flex-col justify-between p-4 leading-normal">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><?php echo $cat['cat_name']; ?></h5>
@@ -58,6 +58,7 @@ foreach($fourcategories as $cat){?>
             $result = $wikiOBJ->get3Wikis();
             foreach($result as $article){
         ?>
+    
     <div
       class="transition-all duration-150 flex w-full px-4 py-6 md:w-1/2 lg:w-1/3"
     >
@@ -79,7 +80,7 @@ foreach($fourcategories as $cat){?>
         </div>
         <hr class="border-gray-300" />
         <div class="flex flex-wrap items-center flex-1 px-4 py-1 text-center mx-auto">
-          <a href="#" class="hover:underline">
+          <a href="categories.php" class="hover:underline">
             <h2 class="text-2xl font-bold tracking-normal text-gray-800">
             <?php echo $article['titre']; ?>
             </h2>
@@ -106,6 +107,7 @@ foreach($fourcategories as $cat){?>
         </section>
       </div>
     </div>
+    
     
     
     </div>
